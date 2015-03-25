c3075a568ab548623b7abb7a37cc2816d3fd6a3f
Joseph Ray <joseph.h.ray@gmail.com>
2015-03-25 15:45:31 +1000

    fixed stuff

--- combinations.py
+++ combinations.py
@@ -4,57 +4,57 @@ class Combinations(object):
     ''' a class for dealing with combinational logic: Adders, decoders, multiplexers. To be used with Bin_nums'''
 
     def __init__(self):
-        pass
+        self.Logic = binary.Logic
         
-    def onebit_adder(self, bin, carry):
+    def onebit_adder(self, bin_a, bin_b, carry):
         "logical implementation of an unsigned adder"
-        if not ((self.bit, bin.bit, carry.bit) == (1, 1, 1)):
+        if not ((bin_a.bit, bin_b.bit, carry.bit) == (1, 1, 1)):
             print "Did nothing, numbers weren't 1 bit"
             return
         else:
             #stage 1
-            spart = self.logicxor(bin)
-            cout1 = self.logicand(bin)
+            spart = self.Logic.Xor(bin_a, bin_b)
+            cout1 = self.Logic.And(bin_a, bin_b)
 
             #stage 2
-            s = spart.logicxor(carry)
-            cout2 = spart.logicand(carry)
+            s = self.Logic.Xor(spart, carry)
+            cout2 = self.Logic.And(spart, carry)
 
             #stage 3
-            carry_out = cout1.logicor(cout2)
+            carry_out = self.Logic.Or(cout1, cout2)
 
-            return (s,carry_out)
+            return (s, carry_out)
 
-    def nbit_adder(self, bin):
-        if self.bit != bin.bit:
+    def nbit_adder(self, bin_a, bin_b):
+        if bin_a.bit != bin_b.bit:
             print "numbers must be the same bit"
             return
         else:
-            carry = Bin_num(binary = [0])
-            n = self.bit
+            carry = binary.Number(binary = [0])
+            n = bin_a.bit
             ans = []
             for i in range(n)[::-1]:
-                a = Bin_num(binary = [self.binvalue[i]])
-                b = Bin_num(binary = [bin.binvalue[i]])
-                (s, carry) = a.onebit_adder(b, carry)
+                a = binary.Number(binary = [bin_a.binvalue[i]])
+                b = binary.Number(binary = [bin_b.binvalue[i]])
+                (s, carry) = self.onebit_adder(a, b, carry)
         
                 ans.insert(0, s.binvalue[0])
-            ansbin = Bin_num(binary = ans)
+            ansbin = binary.Number(binary = ans)
 
             return ansbin, carry
 
-    def nbit_adder2s(self,bin):
+    def nbit_adder2s(self, bin_a, bin_b):
         '''detects overflow for 2s complement addition NOT CURRENTLY WORKING'''
-        (s, carry) = self.nbit_adder(bin)
+        (s, carry) = self.nbit_adder(bin_a, bin_b)
         
-        a = Bin_num(binary = [self.binvalue[0]])
-        b = Bin_num(binary = [bin.binvalue[0]])
+        a = binary.Number(binary = [bin_a.binvalue[0]])
+        b = binary.Number(binary = [bin_b.binvalue[0]])
 
-        ab = a.logicxor(b)
-        ab = ab.logicnot()
+        ab = self.Logic.Xor(a, b)
+        ab = self.Logic.Not(ab) # We check that a and b's MSB are the same using XNOR
 
         print a.binvalue, b.binvalue
         print ab.binvalue, carry.binvalue
-        overflow = carry.logicxor(ab)
+        overflow = self.Logic.Xor(ab,carry) # We check that the carry is different to a and b
 
         return (s, overflow)
--- main.py
+++ main.py
@@ -5,7 +5,10 @@ Conv = converter.ConverterClass()
 Comb = combinations.Combinations()
 
 if __name__ == "__main__":
-    bin1 = binary.Number(binary = [0,0,1,1])
-    bin2 = binary.Number(binary = [0,1,0,1])
+    bin1 = binary.Number(binary = [0,1,0,1])
+    bin2 = binary.Number(binary = [0,0,1,1])
+    (s,overflow) = Comb.nbit_adder2s(bin1, bin2)
+    s.prettyprint()
+    overflow.prettyprint()
     # bin2 = binary.Number(binary = [0,0,1,1])
     Logic.Nand(bin1,bin2).prettyprint()
